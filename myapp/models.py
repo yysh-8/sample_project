f3d5d4e43ff28d455ee68606c94b10b5f41fe531
Youssef Sherif <youssef1.sherif@orange.com>
2022-06-15 15:50:41 +0200

    added a User model and registered it to admin dashboard

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.core.validators import MinValueValidator,MaxValueValidator
 
 # Create your models here.
+class User(models.Model):
+    name=models.CharField(max_length=50)
+    age=models.IntegerField(validators=[MinValueValidator(5),MaxValueValidator(100)])
+
